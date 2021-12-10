5d438a22891fca646e8e12ba36146490068cb4c4
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-10 12:27:55 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -4,7 +4,7 @@ from bot import user_time
 from config import youtube_next_fetch
 from helper.ytdlfunc import extractYt, create_buttons
 
-ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
 
 
 @Client.on_message(Filters.regex(pattern=".*http.*"))
